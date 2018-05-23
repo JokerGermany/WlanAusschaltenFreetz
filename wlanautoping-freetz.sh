148f09bf370befc5ff0f20910db68be966395e0a
JokerGermany <30293477+JokerGermany@users.noreply.github.com>
2018-05-23 08:38:50 +0200

    +ping
    Wenn Lan 1 eingesteckt und ein bestimmte IP Pingbar: Wlan an
    sonst: aus

---
+++ wlanautoping-freetz.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+while [ true ]
+do
+    ping -q -w 2 192.168.222.1 1>&2 2>/dev/null
+    while [ $? == 0 && ctlmgr_ctl r eth0 status/carrier == 1 ]
+    do
+        if [ ctlmgr_ctl r wlan settings/ap_enabled_scnd == 0 ]
+        then
+            ctlmgr_ctl w wlan settings/ap_enabled_scnd 1
+        fi            
+        if [ ctlmgr_ctl r wlan settings/ap_enabled == 0 ]
+        then
+            ctlmgr_ctl w wlan settings/ap_enabled 1
+        fi 
+        sleep 60
+        ping -q -w 2 192.168.222.1 1>&2 2>/dev/null
+    done
+    while [ ctlmgr_ctl r eth0 status/carrier == 0 ]
+    do
+        if [ ctlmgr_ctl r wlan settings/ap_enabled_scnd == 1 ]
+        then
+            ctlmgr_ctl w wlan settings/ap_enabled_scnd 0
+        fi            
+        if [ ctlmgr_ctl r wlan settings/ap_enabled == 1 ]
+        then
+            ctlmgr_ctl w wlan settings/ap_enabled 0
+        fi   
+        sleep 60    
+    done
+done  
+
+
