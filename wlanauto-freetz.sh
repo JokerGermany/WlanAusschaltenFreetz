90a3b00749ff32b9b812cf1641021b71d04062c1
JokerGermany <30293477+JokerGermany@users.noreply.github.com>
2018-05-23 08:32:37 +0200

    Wenn Lan1 an/aus, Wlan an/aus
    Wenn Lan1 ein-/ausgesteckt, Wlan an/aus

---
+++ wlanauto-freetz.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+while [ true ]
+do
+    while [ ctlmgr_ctl r eth0 status/carrier == 1 ]
+    do
+        if [ ctlmgr_ctl r wlan settings/ap_enabled_scnd == 0 ]
+        then
+            ctlmgr_ctl w wlan settings/ap_enabled_scnd 1
+        fi            
+        if [ ctlmgr_ctl r wlan settings/ap_enabled == 0 ]
+        then
+            ctlmgr_ctl w wlan settings/ap_enabled 1
+        fi 
+        sleep 60
+    done
+    while [ ctlmgr_ctl r eth0 status/carrier == 0 ]
+    do
+        if [ ctlmgr_ctl r wlan settings/ap_enabled_scnd == 1 ]
+        then
+            ctlmgr_ctl w wlan settings/ap_enabled_scnd 0
+        fi            
+        if [ ctlmgr_ctl r wlan settings/ap_enabled == 1 ]
+        then
+            ctlmgr_ctl w wlan settings/ap_enabled 0
+        fi   
+        sleep 60    
+    done
+done  
+
+
