1f3b652859ff327397862046e4107adddb569be5
JokerGermany <30293477+JokerGermany@users.noreply.github.com>
2018-05-23 14:39:39 +0200

    fixes

--- wlanautoping-freetz.sh
+++ wlanautoping-freetz.sh
@@ -3,26 +3,26 @@ ip=192.168.222.1
 while [ true ]
 do
     ping -q -w 2 $ip 1>&2 2>/dev/null
-    while [ $? == 0 && ctlmgr_ctl r eth0 status/carrier == 1 ]
+    while [ $? == 0 ] && [ $(ctlmgr_ctl r eth0 status/carrier) == 1 ]
     do
-        if [ ctlmgr_ctl r wlan settings/ap_enabled_scnd == 0 ]
+        if [ $(ctlmgr_ctl r wlan settings/ap_enabled_scnd) == 0 ]
         then
             ctlmgr_ctl w wlan settings/ap_enabled_scnd 1
         fi            
-        if [ ctlmgr_ctl r wlan settings/ap_enabled == 0 ]
+        if [ $(ctlmgr_ctl r wlan settings/ap_enabled) == 0 ]
         then
             ctlmgr_ctl w wlan settings/ap_enabled 1
         fi 
-        sleep 60
+        sleep 30
         ping -q -w 2 $ip 1>&2 2>/dev/null
     done
-    while [ ctlmgr_ctl r eth0 status/carrier == 0 ]
+    while [ $(ctlmgr_ctl r eth0 status/carrier) == 0 ]
     do
-        if [ ctlmgr_ctl r wlan settings/ap_enabled_scnd == 1 ]
+        if [ $(ctlmgr_ctl r wlan settings/ap_enabled_scnd) == 1 ]
         then
             ctlmgr_ctl w wlan settings/ap_enabled_scnd 0
         fi            
-        if [ ctlmgr_ctl r wlan settings/ap_enabled == 1 ]
+        if [ $(ctlmgr_ctl r wlan settings/ap_enabled) == 1 ]
         then
             ctlmgr_ctl w wlan settings/ap_enabled 0
         fi   
